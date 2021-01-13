7c460886228f1b00e8a63828b85a43ec0501c6fa
roehrs1 <roehrs1@uni-bremen.de>
2021-01-13 15:34:03 +0100

    Sketch #3
    sketch #3

--- sketch.js
+++ sketch.js
@@ -4,21 +4,24 @@ const map = new mapboxgl.Map({
 container: 'map', // container id
 style: 'mapbox://styles/experimentalmobileplay/ckjvg4ijw0m6117o2iy47zi5u', // style URL
 center: [8.8017, 53.0793], // starting position in Bremen [lng, lat]
-zoom: 9 // starting zoom
+zoom: 12 // starting zoom
 });
+
+// Add Fullscreen Button
+map.addControl(new mapboxgl.FullscreenControl());
 
 // Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
 
-// Add geolocate control to the map.
-// map.addControl(
-//     new mapboxgl.GeolocateControl({
-//         positionOptions: {
-//             enableHighAccuracy: true
-//         },
-//         trackUserLocation: true
-//     })
-// );
+//Add geolocate control to the map.
+map.addControl(
+    new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+        trackUserLocation: true
+    })
+);
 
 
 // function updatePosition() {
