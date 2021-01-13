46f437414bcd11dc66e7f4c0969eb8d8360f1e8f
roehrs1 <roehrs1@uni-bremen.de>
2021-01-13 15:28:08 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,24 +1,24 @@
 //Map
 mapboxgl.accessToken = 'pk.eyJ1IjoiZXhwZXJpbWVudGFsbW9iaWxlcGxheSIsImEiOiJja2p2Y2xydTIwN2s0MndvYWpmazB4M2IzIn0.q3CYZLs_taS8F7-pA1eF7g';
-var map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
 container: 'map', // container id
 style: 'mapbox://styles/experimentalmobileplay/ckjvg4ijw0m6117o2iy47zi5u', // style URL
 center: [8.8017, 53.0793], // starting position in Bremen [lng, lat]
 zoom: 9 // starting zoom
 });
 
-// Add zoom and rotation controls to the map
+// Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
 
 // Add geolocate control to the map.
-map.addControl(
-    new mapboxgl.GeolocateControl({
-        positionOptions: {
-            enableHighAccuracy: true
-        },
-        trackUserLocation: true
-    })
-);
+// map.addControl(
+//     new mapboxgl.GeolocateControl({
+//         positionOptions: {
+//             enableHighAccuracy: true
+//         },
+//         trackUserLocation: true
+//     })
+// );
 
 
 // function updatePosition() {
