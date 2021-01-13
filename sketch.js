b79c55c425b52388995c3b8bbc4c5ef057602b6c
roehrs1 <roehrs1@uni-bremen.de>
2021-01-13 17:11:42 +0100

    Letzter Stand 13.01.
    Letzter Upload

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-//Map
+// Map
 mapboxgl.accessToken = 'pk.eyJ1IjoiZXhwZXJpbWVudGFsbW9iaWxlcGxheSIsImEiOiJja2p2Y2xydTIwN2s0MndvYWpmazB4M2IzIn0.q3CYZLs_taS8F7-pA1eF7g';
 const map = new mapboxgl.Map({
 container: 'map', // container id
@@ -13,7 +13,7 @@ map.addControl(new mapboxgl.FullscreenControl());
 // Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
 
-//Add geolocate control to the map.
+// Add geolocate control to the map.
 map.addControl(
     new mapboxgl.GeolocateControl({
         positionOptions: {
@@ -23,103 +23,4 @@ map.addControl(
     })
 );
 
-
-
-//Test
-var size = 200;
- 
-// implementation of CustomLayerInterface to draw a pulsing dot icon on the map
-var pulsingDot = {
-width: size,
-height: size,
-data: new Uint8Array(size * size * 4),
- 
-// get rendering context for the map canvas when layer is added to the map
-onAdd: function () {
-var canvas = document.createElement('canvas');
-canvas.width = this.width;
-canvas.height = this.height;
-this.context = canvas.getContext('2d');
-},
- 
-// called once before every frame where the icon will be used
-render: function () {
-var duration = 1000;
-var t = (performance.now() % duration) / duration;
- 
-var radius = (size / 2) * 0.3;
-var outerRadius = (size / 2) * 0.7 * t + radius;
-var context = this.context;
- 
-// Äußerer Kreis
-context.clearRect(0, 0, this.width, this.height);
-context.beginPath();
-context.arc(
-this.width / 2,
-this.height / 2,
-outerRadius,
-0,
-Math.PI * 2
-);
-context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')';
-context.fill();
- 
-// Innerer Kreis
-context.beginPath();
-context.arc(
-this.width / 2,
-this.height / 2,
-radius,
-0,
-Math.PI * 2
-);
-context.fillStyle = 'rgba(255, 100, 100, 1)';
-context.strokeStyle = 'white';
-context.lineWidth = 2 + 4 * (1 - t);
-context.fill();
-context.stroke();
- 
-// update this image's data with data from the canvas
-this.data = context.getImageData(
-0,
-0,
-this.width,
-this.height
-).data;
- 
-// continuously repaint the map, resulting in the smooth animation of the dot
-map.triggerRepaint();
- 
-// return `true` to let the map know that the image was updated
-return true;
-}
-};
- 
-map.on('load', function () {
-map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
- 
-map.addSource('points', {
-'type': 'geojson',
-'data': {
-'type': 'FeatureCollection',
-'features': [
-{
-'type': 'Feature',
-'geometry': {
-'type': 'Point',
-'coordinates': [0, 0]
-}
-}
-]
-}
-});
-map.addLayer({
-'id': 'points',
-'type': 'symbol',
-'source': 'points',
-'layout': {
-'icon-image': 'pulsing-dot'
-}
-});
-});
-
+// Marker
