fee84117ab4410b83366b6b2b9441bbe02b8c8ae
roehrs1 <roehrs1@uni-bremen.de>
2021-01-14 18:14:39 +0100

    Sidebar
    Sidebar

--- sketch.js
+++ sketch.js
@@ -23,6 +23,13 @@ map.addControl(
     })
 );
 
+// Center the map on the coordinates of any clicked symbol from the 'symbols' layer.
+map.on('click', 'Bremen', 'Schnoor', 'Mhle', function (e) {
+    map.flyTo({
+    center: e.features[0].geometry.coordinates
+    });
+});
+
 // // Show markers
 // map.on('click', function(e) {
 //     var features = map.queryRenderedFeatures(e.point, {
@@ -44,11 +51,47 @@ map.addControl(
 //       .addTo(map);
 //   });
   
-//sidebar
-/*
 
-*/
-// 
+
+//sidebar
+new mapboxgl.Marker().setLngLat(center).addTo(map);
+ 
+function toggleSidebar(id) {
+var elem = document.getElementById(id);
+var classes = elem.className.split(' ');
+var collapsed = classes.indexOf('collapsed') !== -1;
+ 
+var padding = {};
+ 
+if (collapsed) {
+// Remove the 'collapsed' class from the class list of the element, this sets it back to the expanded state.
+classes.splice(classes.indexOf('collapsed'), 1);
+ 
+padding[id] = 300; // In px, matches the width of the sidebars set in .sidebar CSS class
+map.easeTo({
+padding: padding,
+duration: 1000 // In ms, CSS transition duration property for the sidebar matches this value
+});
+} else {
+padding[id] = 0;
+// Add the 'collapsed' class to the class list of the element
+classes.push('collapsed');
+ 
+map.easeTo({
+padding: padding,
+duration: 1000
+});
+}
+ 
+// Update the class list on the element
+elem.className = classes.join(' ');
+}
+ 
+map.on('load', function () {
+toggleSidebar('left');
+});
+
+    
 // map.on('load', function () {
 //     // add source and layer for markers
 //     map.addSource('museums', {
