82b0608779c8c71d56ee9e36a349fb2b20422a7b
roehrs1 <roehrs1@uni-bremen.de>
2021-01-19 15:06:42 +0100

    update #1
    update #1

--- sketch.js
+++ sketch.js
@@ -86,7 +86,7 @@ map.on('click', function(e) {
       .addTo(map);
   });
 
-   // Center the map on the coordinates of any clicked symbol from the 'Roland' layer.
+    // Center the map on the coordinates of any clicked symbol from the 'Roland' layer.
     map.on('click', 'Schnoor', function (e) {
         // Center
         map.flyTo({
@@ -107,23 +107,6 @@ map.on('click', function(e) {
         center: e.features[0].geometry.coordinates
         });
     });  
-
-// // Pop-Up Markers
-// map.on('click', function(e) {
-//     let f = map.queryRenderedFeatures(e.point, { layers: ['Roland'] });
-//     if (f.length) {
-//       console.log(f[0]);
-//       return;
-//     } 
-//     f = map.queryRenderedFeatures(e.point, { layers: ['Schnoor'] });
-//     if (f.length) {
-//       console.log(f[0]);
-//       return;
-//     }
-//     f = map.queryRenderedFeatures(e.point, { layers: ['Muehle'] });
-//     if (f.length) {
-//       console.log(f[0]);
-//     }
     
 //sidebar
 //new mapboxgl.Marker().setLngLat(center).addTo(map);
@@ -160,7 +143,7 @@ elem.className = classes.join(' ');
  
 map.on('load', function () {
 // Start the animation.
-  rotateCamera(0);
+//  rotateCamera(0);
 // Sidebar
     toggleSidebar('left');
 // Menu
@@ -237,48 +220,3 @@ for (var i = 0; i < toggleableLayerIds.length; i++) {
     var layers = document.getElementById('menu');
     layers.appendChild(link);
 }
-
-        // Game Start Screen
-        let start = document.querySelector('#brand');
-        let ex = 10;
-        function swing(element) {
-
-        function update(time) {
-            
-            const x = Math.sin(time / 1231) * ex;
-            const y = Math.sin(time / 1458) * ex;
-
-            element.style.transform = [
-                `rotateX(${x}deg)`,
-                `rotateY(${y}deg)`
-            ].join(' ');
-
-            requestAnimationFrame(update);
-        }
-        update(0); 
-        }
-
-        swing(start);
-
-
-        let start_button = start.querySelector('a');
-        let og_color = start_button.style.color;
-        let inter = 0;
-
-        start.addEventListener('mouseover', (e) => {
-        
-        ex = 20;  
-        inter = setInterval(()=>{  
-            start_button.style.color = '#'+Math.floor(Math.random()*16777215).toString(16); 
-        }, 1000); 
-        
-        });
-
-
-        start.addEventListener('mouseout', (e) => {
-        
-        ex = 10;
-        clearInterval(inter);
-        start_button.style.color = og_color; 
-        
-         });
