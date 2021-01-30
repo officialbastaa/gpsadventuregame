b1e15c95ac58d48a0b108da93453d92f1ae51393
roehrs1 <roehrs1@uni-bremen.de>
2021-01-30 23:43:37 +0100

    glitch effect
    glitch effect

--- sketch.js
+++ sketch.js
@@ -9,43 +9,38 @@ center: center,
 });
 
 // Add geolocate control to the map.
-map.addControl(
-    new mapboxgl.GeolocateControl({
-        positionOptions: {
-            enableHighAccuracy: true
-        },
-        trackUserLocation: true
-    })
-);
+var geolocate = new mapboxgl.GeolocateControl();
 
-// Radnom User_ID
-var usernumber = function getSixDigitRandom() {
-  document.getElementById("user").innerHTML =  Math.random().toString().substring(2, 8);
-}
+map.addControl(geolocate);
+
+geolocate.on('geolocate', function(e) {
+      var lon = e.coords.longitude;
+      var lat = e.coords.latitude
+      var userPosition = [lon, lat];
+      console.log(userPosition);
+});
 
 // Timer
-var sec = 0;
+// var sec = 0;
 
-function pad(val) {
-    return val > 9 ? val : "0" + val;
-}
-var timer = setInterval(function () {
-    document.getElementById("seconds").innerHTML = pad(++sec % 60);
-    document.getElementById("minutes").innerHTML = pad(parseInt(sec / 60, 10));
-}, 1000);
+// function pad(val) {
+//     return val > 9 ? val : "0" + val;
+// }
+// var timer = setInterval(function () {
+//     document.getElementById("seconds").innerHTML = pad(++sec % 60);
+//     document.getElementById("minutes").innerHTML = pad(parseInt(sec / 60, 10));
+// }, 1000);
 
-setTimeout(function () {
-    clearInterval(timer);
-}, 1110000);
+// setTimeout(function () {
+//     clearInterval(timer);
+// }, 1110000);
 
 
 map.on('load', function () {
-  // Start the animation.
-  //  rotateCamera(0);
 
   // Sidebar
   toggleSidebar('left');
-
+    
   // Roland (Source and Layer)
   map.addLayer({
     'id': 'MISSION 3',
@@ -106,102 +101,24 @@ map.on('load', function () {
     'source-layer': 'Test'
   });
 
-  // Interactive marker (1)
+  // Interactive marker
   map.on('click', function(e) {
     var features = map.queryRenderedFeatures(e.point, {
-      layers: ['MISSION 1'] 
+      layers: ['MISSION 1','MISSION 2','MISSION 3', 'destination', 'test'] 
     });
-  
+    
     if (!features.length) {
       return;
     }
-    
+      
     var feature = features[0];
-  
+    
     var popup = new mapboxgl.Popup({ offset: [0, -15] })
     .setLngLat(feature.geometry.coordinates)
     .setHTML('<h3>' + feature.properties.title + '</h3><p>' + feature.properties.description + '</p>')
     .addTo(map);
   });
   
-  // Interactive marker (2)
-  map.on('click', function(e) {
-    var features = map.queryRenderedFeatures(e.point, {
-      layers: ['MISSION 2'] 
-    });
-  
-    if (!features.length) {
-      return;
-    }
-    
-    var feature = features[0];
-  
-    var popup = new mapboxgl.Popup({ offset: [0, -15] })
-    .setLngLat(feature.geometry.coordinates)
-    .setHTML('<h3>' + feature.properties.title + '</h3><p>' + feature.properties.description + '</p>')
-    .addTo(map);
-  });
-  
-  // Interactive marker (3)
-  map.on('click', function(e) {
-    var features = map.queryRenderedFeatures(e.point, {
-      layers: ['MISSION 3'] 
-    });
-  
-    if (!features.length) {
-      return;
-    }
-    
-    var feature = features[0];
-  
-    var popup = new mapboxgl.Popup({ offset: [0, -15] })
-    .setLngLat(feature.geometry.coordinates)
-    .setHTML('<h3>' + feature.properties.title + '</h3><p>' + feature.properties.description + '</p>')
-    .addTo(map);
-  });
-
-  // Stop Timer when destination reached
-  map.on('click', 'destination', function(e) {
-    clearInterval(timer);
-  });
-
-  // Interactive marker (Ziele)
-  map.on('click', function(e) {
-
-    var features = map.queryRenderedFeatures(e.point, {
-      layers: ['destination'] 
-    });
-  
-    if (!features.length) {
-      return;
-    }
-    
-    var feature = features[0];
-  
-    var popup = new mapboxgl.Popup({ offset: [0, -15] })
-    .setLngLat(feature.geometry.coordinates)
-    .setHTML('<h3>' + feature.properties.title + '</h3><p>' + feature.properties.description + '</p>')
-    .addTo(map);
-  });
-
-  // Interactive marker (Test)
-  map.on('click', function(e) {
-    var features = map.queryRenderedFeatures(e.point, {
-      layers: ['test'] 
-    });
-  
-    if (!features.length) {
-      return;
-    }
-    
-    var feature = features[0];
-  
-    var popup = new mapboxgl.Popup({ offset: [0, -15] })
-    .setLngLat(feature.geometry.coordinates)
-    .setHTML('<h3>' + feature.properties.title + '</h3><p>' + feature.properties.description + '</p>')
-    .addTo(map);
-  });
-
   // Timer stop when Ziel erreicht
   map.on('click', 'destination', function(e) {
     clearInterval(timer);
@@ -241,21 +158,6 @@ map.on('load', function () {
       center: e.features[0].geometry.coordinates
     });
   });  
-
-  // Change the cursor to a pointer when the it enters a feature in the 'symbols' layer.
-  map.on('mouseenter', 'symbols', function () {
-    map.getCanvas().style.cursor = 'pointer';
-  });
-   
-  // Change it back to a pointer when it leaves.
-  map.on('mouseleave', 'symbols', function () {
-    map.getCanvas().style.cursor = '';
-  });
-
-// geocoder.on('result', function(ev) {
-//   var searchResult = ev.result.geometry;
-//   // Code for the next step will go here
-//   });  
 });
 
 //Menu --------------------------------!>
@@ -279,6 +181,21 @@ var toggleableLayerIds = ['MISSION 1', 'MISSION 2', 'MISSION 3'];
               if (clickedLayer === toggleableLayerIds[j]) {
                 layers.children[j].className = 'active';
                 map.setLayoutProperty(toggleableLayerIds[j], 'visibility', 'visible');
+                
+                // Timer
+                var sec = 0;
+
+                function pad(val) {
+                    return val > 9 ? val : "0" + val;
+                }
+                var timer = setInterval(function () {
+                    document.getElementById("seconds").innerHTML = pad(++sec % 60);
+                    document.getElementById("minutes").innerHTML = pad(parseInt(sec / 60, 10));
+                }, 1000);
+
+                setTimeout(function () {
+                    clearInterval(timer);
+                }, 1110000);
               }
               else {
                 layers.children[j].className = '';
@@ -293,49 +210,7 @@ var toggleableLayerIds = ['MISSION 1', 'MISSION 2', 'MISSION 3'];
       
   }
 
- 
-// set up the corresponding toggle button for each layer
-// for (var i = 0; i < toggleableLayerIds.length; i++) {
-//   var id = toggleableLayerIds[i];
- 
-//   var link = document.createElement('a');
-//     link.href = '#';
-//     link.className = 'active';
-//     link.textContent = id;
- 
-  // link.onclick = function (e) {
-  //   var clickedLayer = this.textContent;
-  //   e.preventDefault();
-  //   e.stopPropagation();
- 
-  //   var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
- 
-  //   // toggle layer visibility by changing the layout object's visibility property
-  //   if (visibility === 'visible') {
-  //     map.setLayoutProperty(clickedLayer, 'visibility', 'none');
-  //     this.className = '';
-  //   } else {
-  //     this.className = 'active';
-  //     map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
-  //   }
-  // };
- 
-//   var layers = document.getElementById('menu');
-//     layers.appendChild(link);
-// }
-
-// // Camera rotation
-// function rotateCamera(timestamp) {
-//   // clamp the rotation between 0 -360 degrees
-//   // Divide timestamp by 100 to slow rotation to ~10 degrees / sec
-//   map.rotateTo((timestamp / 200) % 360, { duration: 0 });
-//   // Request the next frame of the animation.
-//   requestAnimationFrame(rotateCamera);
-//   }
-      
-//sidebar
-//new mapboxgl.Marker().setLngLat(center).addTo(map);
-
+//Sidebar
 function toggleSidebar(id) {
   var elem = document.getElementById(id);
   var classes = elem.className.split(' ');
@@ -344,7 +219,7 @@ function toggleSidebar(id) {
   var padding = {};
  
   if (collapsed) {
-  // Remove the 'collapsed' class from the class list of the element, this sets it back to the expanded state.
+    // Remove the 'collapsed' class from the class list of the element, this sets it back to the expanded state.
     classes.splice(classes.indexOf('collapsed'), 1);
  
     padding[id] = 300; // In px, matches the width of the sidebars set in .sidebar CSS class
@@ -362,27 +237,26 @@ function toggleSidebar(id) {
       duration: 1000
     });
   }
- 
+
   // Update the class list on the element
   elem.className = classes.join(' ');
 }
 
-
 // Missions
 var missions = document.getElementById("myMissions");
 
-// Get the button that opens the Missions
+// erzeugt den Btn zum öffnet des Fenster
 var missions_btn = document.getElementById("myMissionsBtn");
 
-// Get the <span> element that closes the Missions
+// erzeugt das <span> element zum schließen des Fensters
 var span = document.getElementsByClassName("closeMissions")[0];
 
-// When the user clicks the button, open the Missions
+// Beim klicken auf den Btn öffnet sich das Fenster
 missions_btn.onclick = function() {
   missions.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the Missions
+// Beim klicken auf das X schließt sich das Fenster
 span.onclick = function() {
   missions.style.display = "none";
 }
@@ -390,18 +264,14 @@ span.onclick = function() {
 // Story
 var story = document.getElementById("myStory");
 
-// Get the button that opens the Story
 var story_btn = document.getElementById("myStoryBtn");
 
-// Get the <span> element that closes the Story
 var span = document.getElementsByClassName("closeStory")[0];
 
-// When the user clicks the button, open the Story
 story_btn.onclick = function() {
   story.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the Story
 span.onclick = function() {
   story.style.display = "none";
 }
@@ -409,18 +279,14 @@ span.onclick = function() {
 // Help
 var help = document.getElementById("myHelp");
 
-// Get the button that opens the Help
 var help_btn = document.getElementById("myHelpBtn");
 
-// Get the <span> element that closes the Help
 var span = document.getElementsByClassName("closeHelp")[0];
 
-// When the user clicks the button, open the Help
 help_btn.onclick = function() {
   help.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the Help
 span.onclick = function() {
   help.style.display = "none";
 }
